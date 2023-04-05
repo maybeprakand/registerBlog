c0aa8a7e444dcf9a3f8b6aa0e1d6983a2c8680d2
maybeprakand <prakandalama1@gmail.com>
2023-04-05 15:41:37 +0545

    Added middleware

---
+++ app/middlewares/middleware.isvalid.js
@@ -0,0 +1,8 @@
+const isValid = (schema, redirectTo) => (req, res, next) => {
+  const { error } = schema.validate(req.body);
+  if (error) {
+    return res.redirect(redirectTo);
+  }
+  next();
+};
+module.exports = isValid;
--- app/routes/route.auth.js
+++ app/routes/route.auth.js
@@ -2,19 +2,16 @@ const router = require("express").Router();
 
 const authController = require("../controllers/controller.auth");
 
+// validation
+const registerSchema = require("../validator/validator.register");
+const isValid = require("../middlewares/middleware.isvalid");
+
 router.get("/", authController.registerPage);
-
-// backend validation
-
-const schema = require("../validator/validator.register");
-const isValid = router.post("/register", (req, res, next) => {
-  const { error } = schema.validate(req.body);
-  if (error) {
-    return res.redirect("/register");
-  }
-  next();
-});
-router.get("/register", isValid, authController.register);
+router.post(
+  "/register",
+  isValid(registerSchema, "/register"),
+  authController.register
+);
 router.get("/login", authController.loginPage);
 
 module.exports = router;
